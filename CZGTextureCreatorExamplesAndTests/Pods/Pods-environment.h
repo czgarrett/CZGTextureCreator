1a1c27d9fb0d4073f45b2055ffaa7249d03e398f
Chris Garrett <z@zworkbench.com>
2013-05-31 08:16:02 -0400

    Updated to DTCoreText 1.5

---
+++ CZGTextureCreatorExamplesAndTests/Pods/Pods-environment.h
@@ -0,0 +1,44 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DTCoreText
+#define COCOAPODS_POD_AVAILABLE_DTCoreText
+#define COCOAPODS_VERSION_MAJOR_DTCoreText 1
+#define COCOAPODS_VERSION_MINOR_DTCoreText 5
+#define COCOAPODS_VERSION_PATCH_DTCoreText 2
+
+// DTFoundation/Core
+#define COCOAPODS_POD_AVAILABLE_DTFoundation_Core
+#define COCOAPODS_VERSION_MAJOR_DTFoundation_Core 1
+#define COCOAPODS_VERSION_MINOR_DTFoundation_Core 2
+#define COCOAPODS_VERSION_PATCH_DTFoundation_Core 0
+
+// DTFoundation/DTHMLParser
+#define COCOAPODS_POD_AVAILABLE_DTFoundation_DTHMLParser
+#define COCOAPODS_VERSION_MAJOR_DTFoundation_DTHMLParser 1
+#define COCOAPODS_VERSION_MINOR_DTFoundation_DTHMLParser 2
+#define COCOAPODS_VERSION_PATCH_DTFoundation_DTHMLParser 0
+
+// DTFoundation/UIKit
+#define COCOAPODS_POD_AVAILABLE_DTFoundation_UIKit
+#define COCOAPODS_VERSION_MAJOR_DTFoundation_UIKit 1
+#define COCOAPODS_VERSION_MINOR_DTFoundation_UIKit 2
+#define COCOAPODS_VERSION_PATCH_DTFoundation_UIKit 0
+
+// cocos2d
+#define COCOAPODS_POD_AVAILABLE_cocos2d
+#define COCOAPODS_VERSION_MAJOR_cocos2d 2
+#define COCOAPODS_VERSION_MINOR_cocos2d 0
+#define COCOAPODS_VERSION_PATCH_cocos2d 0
+
+// cocos2d/CocosDenshion
+#define COCOAPODS_POD_AVAILABLE_cocos2d_CocosDenshion
+#define COCOAPODS_VERSION_MAJOR_cocos2d_CocosDenshion 2
+#define COCOAPODS_VERSION_MINOR_cocos2d_CocosDenshion 0
+#define COCOAPODS_VERSION_PATCH_cocos2d_CocosDenshion 0
+
